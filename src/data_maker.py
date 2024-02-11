e93fb95f49755481cf686e5dfa8a4408999b9d26
matsumoto <matsumoto.takuya.45e@kyoto-u.jp>
2024-02-11 16:18:22 +0900

    first commit

---
+++ src/data_maker.py
@@ -0,0 +1,6 @@
+import numpy as np
+from sklearn.datasets import load_boston
+
+
+def get_buston_data():
+    return load_boston()
---
+++ src/main.py
@@ -0,0 +1,3 @@
+import numpy as np
+import particles
+from data_maker import get_buston_data
